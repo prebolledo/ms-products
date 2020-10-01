24616cd8fc7ed528a762a412ce18d705c8335b04
Pablo Rebolledo Acosta <pablo.rebolledo.acosta@gmail.com>
2020-10-01 17:24:37 -0300

    [fix] add cors

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ import routes from './routes';
 const bootstrap = () => {
     api(apiOptions,(router, app) => {
         app.use(function(req, res, next) {
+            res.header("Access-Control-Allow-Origin", "*");
+            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
             next();
         }); 
         
